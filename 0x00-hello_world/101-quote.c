219050d1756b9ad73b004057528b8e19f6e64239
Ogunmilade <jesutofunmidavid4@gmail.com>
2023-03-20 20:35:12 -0700

    olumide

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,12 @@
 
 /**
 * main - A c file to display a string to the standard err.
-* Return - 0 signifying program success
+* Return - 0 (signifying program success)
 */
 
 int main(void)
 {
-	write(2 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			stdout);
 	return (1);
+}
